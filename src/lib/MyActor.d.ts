c844c6c9b06dd0076d6e87e0013578ad2c86a82e
Leonardo Ruma <leonardo.ruma@gmail.com>
2022-07-19 09:06:51 -0300

    geração de codigo typescript to JsonLd

---
+++ src/lib/MyActor.d.ts
@@ -0,0 +1,9 @@
+/**
+ * This is a great class!
+ */
+export declare class MyClass {
+    /**
+     * @param paramA - My parameter
+     */
+    constructor(paramA: boolean, paramB: number, paramC: string[]);
+}
---
+++ src/lib/MyActor.ts
@@ -0,0 +1,10 @@
+/**
+ * This is a great class!
+ */
+export class MyClass {
+    /**
+     * @param paramA - My parameter
+     */
+    constructor(paramA: boolean, paramB: number, paramC: string[]) {
+    }
+}
